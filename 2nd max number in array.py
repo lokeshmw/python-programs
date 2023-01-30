ab1aacf5856c1bc10bb6cc4a554797fd8b3645ef
Lokesh M <117714560+lokeshmw@users.noreply.github.com>
2023-01-30 17:25:50 +0530

    Update 2nd max number in array.py

--- 2nd max number in array.py
+++ 2nd max number in array.py
@@ -1,3 +1,4 @@
+# with max
 a = [231, 454, 45, 47, 70, 78, 2334, 555]
 fir_large = a[0]
 sec_large = a[1]
@@ -9,3 +10,12 @@ for i in a:
         sec_large = max(sec_large, i)
 
 print(sec_large)
+
+
+# without max
+a = [231, 454, 45, 47, 70, 78, 2334, 555]
+for i in range(len(a)):
+    for j in range(i+1, len(a)):
+        if a[i] > a[j]:
+            a[i], a[j] = a[j], a[i]
+print(a[-2])
